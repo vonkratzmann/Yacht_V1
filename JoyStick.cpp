9a9600bcf6ad2d3ba52357ca6a9df3d2f7e239a4
kirk <vonkratzmann@gmail.com>
2017-11-27 08:11:04 +1100

    Tidied up doco in joystick code

--- JoyStick.cpp
+++ JoyStick.cpp
@@ -1,28 +1,33 @@
 #include "Arduino.h"
 #include "JoyStick.h"
 
-/** JoyStick constructor
-    initialses current x and y positions to the centre of the joystick
-
-*/
-/* Diagnostics for joystick 
+/* Diagnostics for joystick
  *
  * If set to 1 prints out x and y position on the serial monitor for the joystick
  * Normally set to zero
+ * 
  */
-#define DEBUGJOYSTICK 1
+#define DEBUGJOYSTICK 0
 
+/** JoyStick constructor
+ *
+ *   initialses current x and y positions to the centre of the joystick
+ *
+ */
 JoyStick::JoyStick ()
 {
   x_Cur = y_Cur = 511;                       //10 bit ADC, forwards 0 to 511,backwards 512 to 1023
   x_New = y_New = 511;                       //set joy stick to center position ie stopped
 }
-//end of JoyStick::JoyStick
 
 /** JoyStick check x position
-
-
-*/
+ *
+ *  reads joystick, compares current and new
+ *  masks bottom bits to prevent unwanted noise and doing unnecessary processing
+ *  limits change
+ *  returns true if position has changed
+ *
+ */
 boolean JoyStick::check_X_Pos (void)            //check joystick for any changes
 {
   boolean x_Chnged;                             //flag joy stick position has changed
@@ -40,29 +45,33 @@ boolean JoyStick::check_X_Pos (void)            //check joystick for any changes
     Serial.println(x_New);
 #endif
     diff = x_New - x_Cur;
-    if (abs(diff) > JoyStick_Max_ROC)                    //check if difference greater then max rate of change (ROC)
-    { //limit max acceleration to max rate of change
-      if (diff > 0)                             //check if reading is increasing or decreasing
-        x_Cur += JoyStick_Max_ROC;                       //reading has gone up,so limit acceleration by adding max rate of change
+    if (abs(diff) > JoyStick_Max_ROC)         //check if difference greater then max rate of change (ROC)
+    {                                         //limit max acceleration to max rate of change
+      if (diff > 0)                           //check if reading is increasing or decreasing
+        x_Cur += JoyStick_Max_ROC;            //reading has gone up,so limit acceleration by adding max rate of change
       else
-        x_Cur -= JoyStick_Max_ROC;                       //reading has gone down,so limit acceleration by subtracting max rate of change
+        x_Cur -= JoyStick_Max_ROC;            //reading has gone down,so limit acceleration by subtracting max rate of change
     }
     else
-      x_Cur = x_New;                            // change less than max rate of change, so accept new value
+      x_Cur = x_New;                           // change less than max rate of change, so accept new value
   }
   return x_Chnged;
 }
 //end of JoyStick::chk_X_Pos()
 
 /** JoyStick check y position
-
-
+ *
+ * reads joystick, compares current and new
+ * masks bottom bits to prevent unwanted noise and doing unnecessary processing
+ * limits change
+ * returns true if position has changed
+ *
 */
 boolean JoyStick::check_Y_Pos (void)            //check joystick for any changes
 {
   boolean y_Chnged;                             //flag joy stick position has changed
   y_Chnged = false;                             //reset flag
-  y_New = analogRead(mainsail_JoystickAnalogPin);       //read joystick y position and put into y_new
+  y_New = analogRead(mainsail_JoystickAnalogPin);//read joystick y position and put into y_new
   y_New &= noise_Mask;                          //zero bottom bits to prevent unnecessary calls in case of noise on ADC input
   if ( y_Cur != y_New)                          //Check if changed from last read
   {
@@ -72,18 +81,18 @@ boolean JoyStick::check_Y_Pos (void)            //check joystick for any changes
     Serial.println(y_Cur);
     Serial.print("jsynew: ");
     Serial.println(y_New);
- 
+
 #endif
     diff = y_New - y_Cur;
-    if (abs(diff) > JoyStick_Max_ROC)                    //check if difference greater then max rate of change (ROC)
-    { //limit max acceleration to max rate of change
-      if (diff > 0)                             //check if reading is increasing or decreasing
-        y_Cur += JoyStick_Max_ROC;                       //reading has gone up,so limit acceleration by adding max rate of change
+    if (abs(diff) > JoyStick_Max_ROC)       //check if difference greater then max rate of change (ROC)
+    {                                       //limit max acceleration to max rate of change
+      if (diff > 0)                         //check if reading is increasing or decreasing
+        y_Cur += JoyStick_Max_ROC;          //reading has gone up,so limit acceleration by adding max rate of change
       else
-        y_Cur -= JoyStick_Max_ROC;                       //reading has gone down,so limit acceleration by subtracting max rate of change
+        y_Cur -= JoyStick_Max_ROC;          //reading has gone down,so limit acceleration by subtracting max rate of change
     }
     else
-      y_Cur = y_New;                           // change less than max rate of change, so accept new value
+      y_Cur = y_New;                        // change less than max rate of change, so accept new value
   }
   return y_Chnged;
 }
@@ -93,13 +102,13 @@ boolean JoyStick::check_Y_Pos (void)            //check joystick for any changes
 void JoyStick::process_X(uint8_t &spd, uint8_t &dir)         // process change for x axis of joystick
 {
   /*
-  if (x_Cur <= stopped_High && x_Cur >= stopped_Low)         //check if in the stopped range
-  {
+    if (x_Cur <= stopped_High && x_Cur >= stopped_Low)         //check if in the stopped range
+    {
     spd = 0;                                  //yes, stopped so update speed of motor to say stopped
     dir = forWard;                            //set default direction as forward
-  }
-  else                                        //no, requesting movement
-  {
+    }
+    else                                        //no, requesting movement
+    {
     if (x_Cur < stopped_Low)                  //is joystick asking to move forwards
     { //yes
       dir = forWard;
@@ -110,8 +119,8 @@ void JoyStick::process_X(uint8_t &spd, uint8_t &dir)         // process change f
       dir = backWard;
       spd = map(x_Cur, stopped_High + 1, 1023, minCountSpeed, maxCountSpeed); //Scale joystick position to speed count for stepper
     }
-  }
-*/
+    }
+  */
   return;
 }
 //end of JoyStick::process_X()
@@ -119,14 +128,14 @@ void JoyStick::process_X(uint8_t &spd, uint8_t &dir)         // process change f
 boolean JoyStick::process_Y(uint8_t cur_X_Spd, uint8_t &left_Spd, uint8_t &right_Spd)     // process change for y axis of joystick
 {
   /*
-  turning_Flag = false;
-  left_Spd     = cur_X_Spd;                              //initialise speeds, set left and righ to same, ie moving straight,not turning
-  right_Spd    = cur_X_Spd;
-  if (y_Cur <= stopped_High && y_Cur >= stopped_Low)          //joystcick turning left or right?
-  {
-  }                                                           //no do nothing, leave speed unchanged
-  else                                                        //request to turn
-  {
+    turning_Flag = false;
+    left_Spd     = cur_X_Spd;                              //initialise speeds, set left and righ to same, ie moving straight,not turning
+    right_Spd    = cur_X_Spd;
+    if (y_Cur <= stopped_High && y_Cur >= stopped_Low)          //joystcick turning left or right?
+    {
+    }                                                           //no do nothing, leave speed unchanged
+    else                                                        //request to turn
+    {
     turning_Flag = true;
     if (y_Cur < stopped_Low)                                  //is joystick asking to move left?
     { //yes, slow left wheels & leave right wheels unchanged
@@ -139,11 +148,11 @@ boolean JoyStick::process_Y(uint8_t cur_X_Spd, uint8_t &left_Spd, uint8_t &right
       spd = map(y_Cur, stopped_High + 1, 1023, 0, cur_X_Spd);  //get amount to reduce speed, use y joystick & current x speed
       right_Spd += spd;                                     //slow left wheel speed, by adding 'spd', as a higher count is a lower speed
     }
-  }
+    }
 
-  return turning_Flag;
+    return turning_Flag;
   */
- return;
+  return;
 }
 //end of JoyStick::process_Y()
 
--- JoyStick.h
+++ JoyStick.h
@@ -12,16 +12,23 @@ const uint8_t rudder_JoystickAnalogPin 		= 1;    //x axis of joystick
 const uint8_t mainsail_JoystickAnalogPin 	= 0;    //y xis of joystick
 
 
-/* joystick Parameters */
-const unsigned long JoyStick_Scan_Rate  = 100;      //scan every 100 ms or 1/10 of a second, (Arbirtary choice)
-const unsigned int  JoyStick_Max_ROC    = 511/10;   //limit rate of change allowable by the joystick to 10% (Arbirtary choice)
-const unsigned int  noise_Mask        = 0xFFF0;     //clear bottom bits to mask any noise on signal
+/* joystick Parameters 
+ *
+ * Combination of scan rate and maximum Rate of Change (ROC) limit speed of response of system
+ * Restrict 0 to max change to approximately 2 seconds, ie 0 to 512
+ * as scan rate is 100 millseconds, 
+ * and max change on each joystick scan is 25
+ * 
+ */
+const unsigned long JoyStick_Scan_Rate  = 100;   //scan every 100 ms or 1/10 of a second, (see comments above)
+const unsigned int  JoyStick_Max_ROC    = 25;   //limit rate of change allowable by the joystick (see comments above)
+const unsigned int  noise_Mask          = 0xFFF0; //clear bottom bits to mask any noise on signal
 
 /* regard joystick in stopped position if equal to or between Joys_Stopped_High & Joys_Stopped_Low */
 const int     Joys_Stopped_High = 523;      //setjoystick high range for stopped
 const int     Joys_Stopped_Low  = 500;      //setjoystick low range for stopped
 const int     forWard = 1;                  //set initial direction to forward
-const int     backWard = 0;  
+const int     backWard = 0;
 
 class JoyStick
 {
@@ -30,7 +37,7 @@ class JoyStick
     int     x_New, y_New;            //new position of joystick
     int     diff;
     boolean x_Chnged, y_Chnged;      //flag joy stick position has changed
-    
+
   public:
     JoyStick ();
     boolean    check_X_Pos (void);
